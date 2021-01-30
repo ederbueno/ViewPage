586d1f8800f032ef87cd8c6921465a55027196e6
Ederson Bueno <bueno.ederson@gmail.com>
2021-01-29 21:50:36 -0300

    ViewPage

---
+++ app/src/main/java/com/example/viewpager_exercicio/SectionPagerAdapter.kt
@@ -0,0 +1,18 @@
+package com.example.viewpager_exercicio
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class SectionPagerAdapter(private val fragments: List<Fragment>,
+                          fragmentActivity: FragmentActivity)
+    : FragmentStateAdapter(fragmentActivity){
+
+    override fun getItemCount(): Int {
+        return fragments.size
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return fragments[position]
+    }
+}
